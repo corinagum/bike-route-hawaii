361a76dd434e11c82fd41352a19682a91fb4f7f7
Corina <corina.aj@gmail.com>
2016-02-16 14:13:03 -1000

    init server-side modules and create models files for user and route

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express             = require('express');
+var session             = require('express-session');
+var app                 = express();
+var bodyParser          = require('body-parser');
+var db                  = require('./models');
+var User                = db.User;
+var Task                = db.Task;
+
+app.use(bodyParser.json());
+app.use(express.static('www'));
+
+
+var server = app.listen(process.env.PORT || 4000, function() {
+  db.sequelize.sync();
+});
