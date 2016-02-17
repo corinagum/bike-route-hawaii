ce0e9628b184c8fe8a11869735c09162d1d71c15
Corina <corina.aj@gmail.com>
2016-02-17 13:31:56 -1000

    add passport-facebook to modules for user authentication

--- server/server.js
+++ server/server.js
@@ -4,10 +4,24 @@ var app                 = express();
 var bodyParser          = require('body-parser');
 var db                  = require('./../models/');
 var user                = db.user;
-var route                = db.route;
+var route               = db.route;
+var SECRET              = require('./config/secret.js');
 
 app.use(bodyParser.json());
 app.use(express.static('www'));
+
+passport.use(new FacebookStrategy({
+   clientID: SECRET.FACEBOOK_APP_ID,
+   clientSecret: SECRET.FACEBOOK_APP_SECRET,
+   callbackURL: "http://localhost:4000/auth/facebook/callback"
+ },
+ function(accessToken, refreshToken, profile, cb) {
+   User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+     return cb(err, user);
+   });
+ }
+));
+
 app.use('/users', require('./routes/user-route.js'));
 app.use('/routes', require('./routes/routes-route.js'));
 
