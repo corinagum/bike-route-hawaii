83b28ace53aea4ea90f02711fad44475ef248352
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-19 12:29:52 -1000

    moves android tabs to bottom

--- www/js/app.js
+++ www/js/app.js
@@ -16,7 +16,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  $ionicConfigProvider.tabs.position('bottom');
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
