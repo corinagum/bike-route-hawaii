09201c2708d335a81dc13c319a874a5273aa60f9
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-18 15:24:11 -1000

    Updates gallery view to have thumbnail that goes to more detailed view

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,29 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Paki and Kalakaua',
+    desc: 'Blue bottle slow-carb letterpress, intelligentsia meggings deep v keffiyeh vinyl. Kickstarter helvetica ethical, ennui kogi ramps 90\'s listicle flannel iPhone actually marfa banjo hashtag. Asymmetrical ugh waistcoat, cred green juice photo booth umami meggings. Flannel pinterest dreamcatcher vinyl readymade, tacos scenester austin normcore listicle church-key. Hammock paleo organic four dollar toast. You probably haven\'t heard of them asymmetrical taxidermy, gochujang 90\'s tattooed tofu authentic tousled microdosing iPhone lumbersexual ennui fashion axe gastropub. Pop-up kinfolk brunch, lo-fi ramps squid health goth tilde.',
+    face: 'http://chromecastbg.alexmeub.com/images/240_AF1QipMrVESpBMMxsgHBYtljVZWvhOy1dpmtS-vVCsXX.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Paki and Noela',
+    desc: 'Blue bottle slow-carb letterpress, intelligentsia meggings deep v keffiyeh vinyl. Kickstarter helvetica ethical, ennui kogi ramps 90\'s listicle flannel iPhone actually marfa banjo hashtag. Asymmetrical ugh waistcoat, cred green juice photo booth umami meggings. Flannel pinterest dreamcatcher vinyl readymade, tacos scenester austin normcore listicle church-key. Hammock paleo organic four dollar toast. You probably haven\'t heard of them asymmetrical taxidermy, gochujang 90\'s tattooed tofu authentic tousled microdosing iPhone lumbersexual ennui fashion axe gastropub. Pop-up kinfolk brunch, lo-fi ramps squid health goth tilde.',
+    face: 'http://chromecastbg.alexmeub.com/images/240_AF1QipMAuhpOoerXqmbSvb6MGvZhPAJnkrWmFx3QNe6R.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Kapiolani Park 1',
+    desc: 'Blue bottle slow-carb letterpress, intelligentsia meggings deep v keffiyeh vinyl. Kickstarter helvetica ethical, ennui kogi ramps 90\'s listicle flannel iPhone actually marfa banjo hashtag. Asymmetrical ugh waistcoat, cred green juice photo booth umami meggings. Flannel pinterest dreamcatcher vinyl readymade, tacos scenester austin normcore listicle church-key. Hammock paleo organic four dollar toast. You probably haven\'t heard of them asymmetrical taxidermy, gochujang 90\'s tattooed tofu authentic tousled microdosing iPhone lumbersexual ennui fashion axe gastropub. Pop-up kinfolk brunch, lo-fi ramps squid health goth tilde.',
+    face: 'http://chromecastbg.alexmeub.com/images/240_AF1QipNJ98LLErUYUwfcyu1SjwQX_Wt_3we-Ux3aAytf.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Kapiolani Park 2',
+    desc: 'Blue bottle slow-carb letterpress, intelligentsia meggings deep v keffiyeh vinyl. Kickstarter helvetica ethical, ennui kogi ramps 90\'s listicle flannel iPhone actually marfa banjo hashtag. Asymmetrical ugh waistcoat, cred green juice photo booth umami meggings. Flannel pinterest dreamcatcher vinyl readymade, tacos scenester austin normcore listicle church-key. Hammock paleo organic four dollar toast. You probably haven\'t heard of them asymmetrical taxidermy, gochujang 90\'s tattooed tofu authentic tousled microdosing iPhone lumbersexual ennui fashion axe gastropub. Pop-up kinfolk brunch, lo-fi ramps squid health goth tilde.',
+    face: 'http://chromecastbg.alexmeub.com/images/240_AF1QipNPQtLL5EcPrqxhs07EaSz5ose7xHmmLb9u10BX.jpg'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Waikiki Shell',
+    desc: 'Blue bottle slow-carb letterpress, intelligentsia meggings deep v keffiyeh vinyl. Kickstarter helvetica ethical, ennui kogi ramps 90\'s listicle flannel iPhone actually marfa banjo hashtag. Asymmetrical ugh waistcoat, cred green juice photo booth umami meggings. Flannel pinterest dreamcatcher vinyl readymade, tacos scenester austin normcore listicle church-key. Hammock paleo organic four dollar toast. You probably haven\'t heard of them asymmetrical taxidermy, gochujang 90\'s tattooed tofu authentic tousled microdosing iPhone lumbersexual ennui fashion axe gastropub. Pop-up kinfolk brunch, lo-fi ramps squid health goth tilde.',
+    face: 'http://chromecastbg.alexmeub.com/images/240_AF1QipN0X6n39_rePcGsmL_LgxFbthfIyDHjBeMlsmr5.jpg'
   }];
 
   return {
