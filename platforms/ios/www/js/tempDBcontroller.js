cd551f4117760ca4e56bffc8aae02ab155128f14
Corina <corina.aj@gmail.com>
2016-02-20 13:42:15 -1000

    try adding explicit access to confib.xml in order to display map

---
+++ platforms/ios/www/js/tempDBcontroller.js
@@ -0,0 +1,36 @@
+angular.module('starter.controller', [])
+  .controller('DBcontroller', ['$scope', 'PointService', function($scope, PointService){
+
+    $scope.getPoints = function(){
+      PointService.getPoints()
+        .then(function(data){
+          var geoJSON = {
+            "type" : "FeatureCollection",
+            "features" : []
+          };
+          for(var i=0; i<data.results.length; i++){
+            var point = {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [ data.results[i].long, data.results[i].lat, 0]
+              },
+              "properties" : data.results[i]
+            };
+            geoJSON.features.push(point);
+          }
+        });
+    };
+
+    var geoJSONHistory;
+    var geoJSONBikeShare;
+
+    $scope.getPointsInRadius = function(radius, lat, long){
+      PointService.getPointsInRadius(radius, lat, long)
+        .then(function(data){
+          geoJSONHistory = data.geoJSONHistory;
+          geoJSONBikeShare = data.geoJSONBikeShare;
+        });
+    };
+
+  }]);
