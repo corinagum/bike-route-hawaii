262708962768381c70fe55965a97e50ce26c5d79
Kent Salcedo <kentsalcedo@gmail.com>
2016-03-15 12:17:35 -1000

    attempting to fix pop-up issue

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,7 +26,8 @@
     },
     defaults: {
       scrollWheelZoom: false,
-      // doubleClickZoom: false,
+      doubleClickZoom: false,
+      touchZoom: false;
       inertiaMaxSpeed: 150
     },
     center : {
@@ -307,7 +308,7 @@
   };
 
   // SWITCH FOR TURNING DRAG ON AND OFF
-  $scope.showPointsOnDrag = false;
+  $scope.showPointsOnDrag = true;
   $scope.setShowPointsOnDrag = function(){
     $scope.showPointsOnDrag = !$scope.showPointsOnDrag;
   };
